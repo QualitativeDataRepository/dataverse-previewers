586dc6fc9ec4a92755a60d345b73f869eec739f9
Jim Myers <myersjd@umich.edu>
2021-11-30 12:43:08 -0500

    remove v1.1.1, populate 1.3

--- previewers/v1.1.1/js/video.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-startPreview(false);
-});
-
-function writeContent(fileUrl, file, title, authors) {
-addStandardPreviewHeader(file, title, authors);
-          $(".preview").append($("<video/>").prop("controls",true).append($('<source/>').attr("src",fileUrl)));
-
-}
