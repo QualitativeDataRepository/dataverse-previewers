20b8c68a6f98eb96e1fdcced4ed5f00f8748a86b
Eryk Kulikowski <eryk.kulikowski@kuleuven.be>
2023-02-22 15:46:30 +0100

    convert data to html directly

--- previewers/betatest/js/md.js
+++ previewers/betatest/js/md.js
@@ -7,27 +7,10 @@ function translateBaseHtmlPage() {
     $('.mdPreviewText').text(mdPreviewText);
 }
 
-function writeContent(fileUrl, file, title, authors) {
+function writeContentAndData(data, fileUrl, file, title, authors) {
     addStandardPreviewHeader(file, title, authors);
-    var mdUrl = queryParams.get("siteUrl") + "/api/access/datafile/" + queryParams.get("fileid");
-
-    $.ajax({
-        type: 'GET',
-        dataType: 'text',
-        crosssite: true,
-        url: mdUrl,
-        success: function (data, status) {
-            $('.preview').append($("<textarea/>").html(() => {
-                const showdown = require('showdown');
-                const converter = new showdown.Converter();
-                const html = converter.makeHtml(data);
-                return html;
-            }
-            ).attr('style', 'width:100%;height:100%;')
-            );
-        },
-        error: function (request, status, error) {
-            reportFailure($.i18n("mdErrorText"), error);
-        }
-    });
+    const showdown = require('showdown');
+    const converter = new showdown.Converter();
+    const html = converter.makeHtml(data);
+    $('.preview').append($("<textarea/>").html(html));
 }
