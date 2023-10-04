afd1a0ad824d5bf7deeaa291e81fb6a7cc0d6c92
Eryk Kulikowski <eryk.kulikowski@kuleuven.be>
2023-10-04 16:58:25 +0200

    clarified the origin of ro-crate-dynamic.js

--- previewers/betatest/js/ro-crate-dynamic.js
+++ previewers/betatest/js/ro-crate-dynamic.js
@@ -1,3 +1,8 @@
+/*
+Based on https://github.com/Language-Research-Technology/ro-crate-html-js
+copied from https://unpkg.com/ro-crate-html-js@1.4.19/dist/ro-crate-dynamic.js (resolved https://unpkg.com/ro-crate-html-js/dist/ro-crate-dynamic.js) with added call to load() function at the line 135
+*/
+
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
   /* 
   
