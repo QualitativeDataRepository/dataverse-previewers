8fb283615d150aeea6cbc94fbabe9d5532ea6f5f
Jim Myers <qqmyers@hotmail.com>
2023-12-08 11:32:59 -0500

    Rich Html previewer updates
    Prior code did not handle i18n correctly and would pop up the
    confirmation dialog on the main file page when launching the previewer
    in a separate window, causing the previewer to hang/fail. The return
    option also created a second tab with the dataset. This PR replaces the
    popup with an inline warning and the option to Display With/Without the
    Rich Content, and, if the data doesn't contain scripts directly or a
    <head> element, just displays it as the HTML previewer would (making
    this a possible replacement for the HTML Previewer rather than an
    optional second previewer.) Also ran some auto-translations into es and
    fr.

--- previewers/betatest/js/richhtml.js
+++ previewers/betatest/js/richhtml.js
@@ -1,52 +1,6 @@
-$(document).ready(function () {
-
-    const MESSAGE = $.i18n("richHtmlPreviewText")
-    userConfirms = confirm(MESSAGE)
-
-    if (userConfirms) {
-        // Preview the HTML file
-        startPreview(true);
-    } else {
-        // Redirect to the file page
-        queryParams = new URLSearchParams(window.location.search.substring(1));
-        var siteUrl = queryParams.get("siteUrl");
-        var fileID = queryParams.get("fileid");
-        var versionUrl = siteUrl + "/api/datasets/"
-            + queryParams.get("datasetid") + "/versions/"
-            + queryParams.get("datasetversion");
-
-        fetchMetaAndRedirect(versionUrl, fileID, siteUrl);
-    }
+$(document).ready(function() {
+    startPreview(true);
 });
-
-function fetchMetaAndRedirect(versionURL, fileID, siteUrl) {
-    $.ajax({
-        type: 'GET',
-        dataType: "json",
-        crosssite: true,
-        url: versionURL,
-        success: function (data, status) {
-            console.log(data);
-            redirectToFilePage(data, siteUrl, fileID);
-        },
-        error: function (request, status, error) {
-            alert("Could not find persistent ID for file. Redirecting to the Dataverse page.")
-            window.location.replace(siteUrl);
-        }
-    });
-}
-
-function redirectToFilePage(data, siteUrl, fileID) {
-    // Search for the file ID in the JSON
-    const files = data.data.files
-    const persistentFile = files.find(file => file.dataFile.id == fileID)
-    const persistentFileId = persistentFile.dataFile.persistentId
-    const fileVersion = persistentFile.version
-
-    // Redirect to the file page
-    const fileUrl = siteUrl + "/file.xhtml?persistentId=" + persistentFileId + "&version=" + fileVersion
-    window.location.replace(fileUrl);
-}
 
 function translateBaseHtmlPage() {
     var htmlPreviewText = $.i18n("htmlPreviewText");
@@ -57,7 +11,27 @@ function writeContentAndData(data, fileUrl, file, title, authors) {
     addStandardPreviewHeader(file, title, authors);
     options = {
         "stripIgnoreTag": true,
+        "stripIgnoreTagBody": ['script', 'head']
     };  // Custom rules
+    hasRichContent = false;
+    if (data.includes('<script') || data.includes('<head')) {
+        hasRichContent = true;
+    }
+    if (hasRichContent) {
+        //Add Rich Content warning and choice buttons
+        var header = $('<div/>').insertBefore($('.preview')).addClass('preview-header container center');;
+        url = fileUrl;
+        theData = data;
+        header.append($('<div/>').html($.i18n('richContentWarning')).addClass('center'));
+        subheader = $('<div/>').appendTo(header).addClass('center');
+        subheader.append($("<div/>").addClass("btn btn-default")
+                 .html("<a href=\"javascript:console.log('Click');$('.preview').html(theData);\">" + $.i18n('displayWithRichContent') + "</a>"));
+        subheader.append($("<div/>").addClass("btn btn-default")
+                  .html("<a href=\"javascript:console.log('Click');$('.preview').html(filterXSS(theData,options));\">" + $.i18n('displayWithoutRichContent') + "</a>"));
+    } else {
+        //Display filtered content as for normal HTML Previewer
+        $('.preview').append($("<div/>").html(filterXSS(data)));
+    }
 
-    $('.preview').append($("<div/>").html(data));
 }
+
