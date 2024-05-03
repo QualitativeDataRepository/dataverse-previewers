c3d2a1eda727842fdacaa55e961f073331d4918a
paulboon <paul.boon@dans.knaw.nl>
2024-05-03 14:46:08 +0200

    Cleanup of x3dview.js

--- previewers/betatest/js/x3dview.js
+++ previewers/betatest/js/x3dview.js
@@ -9,16 +9,15 @@ function translateBaseHtmlPage() {
 
 function writeContentAndData(data, fileUrl, file, title, authors) {
     addStandardPreviewHeader(file, title, authors);
-
-    $('.preview').append($("<pre/>").html(fileUrl));
     
+    /** Human readable formats could also be displayed (as text or xml or json).
     options = {};  // Custom rules
     $('.preview').append($("<pre/>").html(filterXSS(data,options)));
+    */
     
     var browser = X3D.getBrowser("x3d-canvas#x3dpreviewcanvas");
-    console.log("Version: " + browser.getVersion());
+    console.log("X3D Browser: " + browser.getName() + " " + browser.getVersion());
+    console.log("Start loading file: " + fileUrl);
     browser.loadURL(new X3D.MFString(fileUrl));
-    //browser.replaceWorld(browser.createX3DFromString(data));
     //browser.viewAll();
-    //browser.reload();
 }
