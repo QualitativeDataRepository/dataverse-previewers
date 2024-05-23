bb94c7eaef1ba8847a3e3b52ec91150c7cfa08d2
Jim Myers <qqmyers@hotmail.com>
2024-05-23 18:19:43 -0400

    add changing example commands

--- localinstall.sh
+++ localinstall.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+dirLocal=$(pwd)
 
 # go to the version folder
 if [ -z "$1" ]; then
-    echo "The variable is empty. Provide the relative path to the previewer version you wish to make an all-local instance of."
+    echo "Provide the relative path to the previewer version you wish to make an all-local instance of, e.g. previewers/v1.4."
     exit 1
 fi
+
+if [ -z "$2" ]; then
+    echo "To update the example curl commands, add the base URL for your local previewer installation as the second parameter."
+    echo "E.g. https://yourinstitution.org/mypreviewerdir to have the Text previewer at https://yourinstitution.org/mypreviewerdir/previewers/v1.4/TextPreview.html"
+    echo "The version will match the relative path you provide"
+    echo ""
+fi
+
 folder="$1"
 cd "${folder}"
 
@@ -17,7 +26,7 @@ cat urls_js.txt
 
 echo Downloading local copies of remote CSS files:
 sed -n 's/.*<link.*href="\(http[^"]*\)".*/\1/p' *.html | sort -u | sed -n 's/^\(.*\/\)*\(.*\)/sed -i \x27s,\0\,lib\/\2,\x27 *.html/p' > replace_css.sh
-sed -n 's/.*<link.*href="\(http[^"]*\)".*/\1/p' *.html | sort -u ?urls_css.txt
+sed -n 's/.*<link.*href="\(http[^"]*\)".*/\1/p' *.html | sort -u > urls_css.txt
 source replace_css.sh
 cat urls_css.txt
 
@@ -30,7 +39,7 @@ while read url; do
 done < "../urls_js.txt"
 
 
-cd "${folder}"
+cd ".."
 if [ ! -d ./css ]; then
   mkdir ./css
 fi
@@ -40,6 +49,16 @@ while read url; do
 done < "../urls_css.txt"
 
 cd ..
+
+
+if [ ! -z "$2" ]; then
+cd "${dirLocal}"
+echo Changing example curl commands to use local URLs
+localurl="$2"
+sed -i "s,https://gdcc.github.io/dataverse-previewers/previewers/v1.[34],$localurl/$folder,g" *curlcommands.md
+echo Done changing example curl commands to use local URLs
+cd "${folder}"
+fi
 
 echo Cleaning Up...
 rm urls_js.txt
