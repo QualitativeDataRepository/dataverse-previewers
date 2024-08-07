b2a37ca0df3c9c3afa10d247854a0e6c54c6bc57
qqmyers <qqmyers@hotmail.com>
2024-08-07 18:53:59 -0400

    Initial Voyager 3D support for .glb files

---
+++ previewers/betatest/js/voyager.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    startPreview(false);
+});
+
+function translateBaseHtmlPage() {
+    var videoPreviewText = $.i18n("videoPreviewText");
+    $('.videoPreviewText').text(videoPreviewText);
+}
+
+function writeContent(fileUrl, file, title, authors) {
+    // Currently the styling of Voyager hides this info anyway.
+    //    addStandardPreviewHeader(file, title, authors);
+    const lastIndexOfChar = fileUrl.lastIndexOf("/");
+    const rootUrl = fileUrl.substring(0, lastIndexOfChar + 1);
+    const fileName = fileUrl.substring(lastIndexOfChar + 1);
+    console.log(fileUrl);
+    $("voyager-explorer").attr("root", rootUrl).attr("model", fileName);
+    $.getScript("lib/voyager-explorer.min.v0.17.0.gdcc.js");
+}
