cc9a62b4e05ccbaa4a70098a0afe42bb9f2f9c82
Eryk Kulikowski <eryk.kulikowski@kuleuven.be>
2025-11-19 10:52:47 +0100

    feat(cdi-preview): add CDIF Discovery Core option and shapes for validation

--- previewers/betatest/js/cdi-preview/core.js
+++ previewers/betatest/js/cdi-preview/core.js
@@ -51,6 +51,7 @@ let hadOriginalGraph = true; // Track if original data had @graph (for export pr
 const SHAPE_URLS = {
   "ddi-cdi-official":
     "https://ddi-cdi.github.io/m2t-ng/DDI-CDI_1-0/encoding/shacl/ddi-cdi.shacl.ttl",
+  "cdif-core": "shapes/cdif-core.ttl",
   "local-fallback": "shapes/ddi-cdi-official.ttl",
 };
 
