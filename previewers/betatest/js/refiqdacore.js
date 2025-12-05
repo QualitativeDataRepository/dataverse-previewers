7863896171c386e8f7a674e75d000ad4bc061877
Jim Myers <qqmyers@hotmail.com>
2025-12-05 14:16:31 -0500

    Source table before annotations

--- previewers/betatest/js/refiqdacore.js
+++ previewers/betatest/js/refiqdacore.js
@@ -316,49 +316,6 @@ if (xmlDoc.getElementsByTagName("Sources")[0]) {
         }
       }
       
-      // Create Annotations table if there are any annotations
-      if (annotationRows.length > 0) {
-        $('#filterby').append($('<option/>').prop('value', 'Annotations').text('Annotations'));
-        
-        let annotationBlock = $('<div/>').width(tableWidth).appendTo($(".preview"));
-        annotationBlock.append($("<h2/>").html("Annotations"));
-        let annotationTable = createTable("Annotations", "Filename", "Type", "Selection", "Codes").appendTo(annotationBlock);
-        annotationTable.addClass("annotationtable compact stripe");
-        
-        annotationRows.forEach(function(rowData) {
-          let tr = addRow(annotationTable, rowData.sourceRef, rowData.type, rowData.name, rowData.codes);
-          tr.attr('data-guid', rowData.guid);
-          tr.attr('data-matches', rowData.matches);
-        });
-        
-        var annotationDataTable = $(".annotationtable").DataTable({
-          select: $('#filterby').val() == 'Annotations'
-        });
-        
-        if (typeof downloadFile === 'function') {
-          $("a[data-entry-index]").click(downloadFile);
-          $('.annotationtable').on('draw.dt', function() {
-            $("a[data-entry-index]").off('click');
-            $("a[data-entry-index]").click(downloadFile);
-          });
-        }
-        
-        tables.push(annotationDataTable);
-        annotationDataTable.on('select deselect', function(e, dt, type, indexes) {
-          if (type === 'row') {
-            var data = annotationDataTable.rows(indexes).data().pluck('id');
-            annotationDataTable[type](indexes).nodes().to$().addClass('custom-selected');
-            console.log('uG: ' + annotationDataTable[type](indexes).nodes().to$().attr('data-guid'));
-            console.log(annotationDataTable.rows({ selected: true }).count());
-            console.log("clearing sG in annotation");
-            selectedGUIDs = new Array();
-            annotationDataTable.rows({ selected: true }).nodes().to$().each(function(index, element) { selectedGUIDs.push(element.dataset.guid) });
-            selectedGUIDs.forEach(guid => { console.log('Added ' + guid); });
-            tables.filter(function(curTable) { return curTable !== annotationDataTable }).forEach(table => { table.draw() });
-          }
-        });
-      }
-      
       // Create Sources table if there are any sources
       if (sourceRows.length > 0) {
         $('#filterby').append($('<option/>').prop('value', 'Sources').text('Sources'));
@@ -398,6 +355,50 @@ if (xmlDoc.getElementsByTagName("Sources")[0]) {
             sourceDataTable.rows({ selected: true }).nodes().to$().each(function(index, element) { selectedGUIDs.push(element.dataset.guid) });
             selectedGUIDs.forEach(guid => { console.log('Added ' + guid); });
             tables.filter(function(curTable) { return curTable !== sourceDataTable }).forEach(table => { table.draw() });
+          }
+        });
+      }
+      
+            
+      // Create Annotations table if there are any annotations
+      if (annotationRows.length > 0) {
+        $('#filterby').append($('<option/>').prop('value', 'Annotations').text('Annotations'));
+        
+        let annotationBlock = $('<div/>').width(tableWidth).appendTo($(".preview"));
+        annotationBlock.append($("<h2/>").html("Annotations"));
+        let annotationTable = createTable("Annotations", "Filename", "Type", "Selection", "Codes").appendTo(annotationBlock);
+        annotationTable.addClass("annotationtable compact stripe");
+        
+        annotationRows.forEach(function(rowData) {
+          let tr = addRow(annotationTable, rowData.sourceRef, rowData.type, rowData.name, rowData.codes);
+          tr.attr('data-guid', rowData.guid);
+          tr.attr('data-matches', rowData.matches);
+        });
+        
+        var annotationDataTable = $(".annotationtable").DataTable({
+          select: $('#filterby').val() == 'Annotations'
+        });
+        
+        if (typeof downloadFile === 'function') {
+          $("a[data-entry-index]").click(downloadFile);
+          $('.annotationtable').on('draw.dt', function() {
+            $("a[data-entry-index]").off('click');
+            $("a[data-entry-index]").click(downloadFile);
+          });
+        }
+        
+        tables.push(annotationDataTable);
+        annotationDataTable.on('select deselect', function(e, dt, type, indexes) {
+          if (type === 'row') {
+            var data = annotationDataTable.rows(indexes).data().pluck('id');
+            annotationDataTable[type](indexes).nodes().to$().addClass('custom-selected');
+            console.log('uG: ' + annotationDataTable[type](indexes).nodes().to$().attr('data-guid'));
+            console.log(annotationDataTable.rows({ selected: true }).count());
+            console.log("clearing sG in annotation");
+            selectedGUIDs = new Array();
+            annotationDataTable.rows({ selected: true }).nodes().to$().each(function(index, element) { selectedGUIDs.push(element.dataset.guid) });
+            selectedGUIDs.forEach(guid => { console.log('Added ' + guid); });
+            tables.filter(function(curTable) { return curTable !== annotationDataTable }).forEach(table => { table.draw() });
           }
         });
       }
