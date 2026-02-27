e44d8aec504370dfffbb068783e18e51d7296c70
Jim Myers <qqmyers@hotmail.com>
2026-02-27 10:37:21 -0500

    initial redacted code

--- previewers/betatest/js/refiqda.js
+++ previewers/betatest/js/refiqda.js
@@ -1,15 +1,20 @@
 function writeContent(fileUrl, file, title, authors) {
-    addStandardPreviewHeader(file, title, authors);
-    options = {
-        "stripIgnoreTag": true,
-        "stripIgnoreTagBody": ['script', 'head']
-    };  // Custom rules
-    wait = $('<div/>').attr('id', 'waiting');
-    $('<img/>').width('15%').attr('src','images/Loading_icon.gif').attr('id','throbber').appendTo(wait);
-    $('<span/>').text('Retrieving File...').appendTo(wait);
-    wait.appendTo($('.preview'));
+  addStandardPreviewHeader(file, title, authors);
+  options = {
+    "stripIgnoreTag": true,
+    "stripIgnoreTagBody": ['script', 'head']
+  };  // Custom rules
+  wait = $('<div/>').attr('id', 'waiting');
+  $('<img/>').width('15%').attr('src', 'images/Loading_icon.gif').attr('id', 'throbber').appendTo(wait);
+  $('<span/>').text('Retrieving File...').appendTo(wait);
+  wait.appendTo($('.preview'));
 
-        fetch(fileUrl)
-  .then(response => response.text())
-  .then(data => parseData(data));
+  if (fileUrl.includes('auxiliary/qdpx')) {
+    redactedMode = true;
+  } else {
+    redactedMode = false;
+  }
+  fetch(fileUrl)
+    .then(response => response.text())
+    .then(data => parseData(data));
 }
